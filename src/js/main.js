2627ca7a47b975c0605730dc62b576ce38127209
Artyom Leontev <lag.win@yandex.ru>
2019-03-03 21:59:22 +0300

    модальное окно через JQuery + добавлена кнопка наверх

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
-var button = document.querySelector('#button');
-var modal = document.querySelector('#modal');
-var close = document.querySelector('#close');
+// var button = document.querySelector('#button');
+// var modal = document.querySelector('#modal');
+// var close = document.querySelector('#close');
 
-button.addEventListener('click', function() {
-  modal.classList.add('modal_active');
-})
+// button.addEventListener('click', function() {
+//   modal.classList.add('modal_active');
+// })
 
-close.addEventListener('click', function(){
-  modal.classList.remove('modal_active');  
-})
+// close.addEventListener('click', function(){
+//   modal.classList.remove('modal_active');  
+// })
 
-setInterval(function(){
-  modal.classList.remove('modal_active');
-},5000);  
-clearTimeout();
+// setInterval(function(){
+//   modal.classList.remove('modal_active');
+// },5000);  
+// clearTimeout();
