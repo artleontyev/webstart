f124c6b0aaba9ef08376c3cdc6d92bc645e95db8
Artyom Leontev <lag.win@yandex.ru>
2019-03-09 11:51:49 +0300

    модальное окно 5 сек скрывается

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,13 +1,14 @@
-$(document).ready(function (){
-  var button = $('#button');
-  var modal = $('#modal');
-  var close = $('#close')
+var button = document.querySelector("#button");
+var modal = document.querySelector("#modal");
+var close = document.querySelector("#close");
 
-  button.on('click', function() {
-    modal.addClass('modal_active')
-  }); 
-  close.on('click', function () {
-    modal.removeClass('modal_active')
+button.addEventListener("click", function start() {
+      modal.classList.add("modal_active")
+      setTimeout(function end() {
+        modal.classList.remove("modal_active")
+      }, 5000)
+      close.addEventListener("click", function end() {
+            modal.classList.remove("modal_active")
     
   });
 });
